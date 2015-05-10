cd43dcce53e28ea8cf972208fd5e5b03b51d0c80
Javier Forero <git@javierforero.com>
2015-05-10 18:19:59 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,12 @@
 ## Getting the Data
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=T, sep=";", stringsAsFactors=FALSE, dec=".")
+dataF <- "./data/household_power_consumption.txt"
+data <- read.table(dataF, header=T, sep=";", stringsAsFactors=F, dec=".", na.strings= "?")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## Subsetting the Data
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
 
-## Plot 1
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power
-(kilowatts)")
+## Creating Plot 1
+png("plot1.png", width=480, height=480)
+hist(data$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab="Frequency", main= "Global Active Power")
 dev.off()
